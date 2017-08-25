b0849dc62b1d1339a2eb77f7f1d28cab5673a417
Henry Paulino <henrypl360@gmail.com>
2017-08-24 21:08:04 -0400

    Eject project
    Adde Post.js and ListPage.js

---
+++ src/Environment.js
@@ -0,0 +1,31 @@
+const {
+  Environment,
+  Network,
+  RecordSource,
+  Store,
+} = require('relay-runtime')
+
+const store = new Store(new RecordSource())
+
+const network = Network.create((operation, variables) => {
+  return fetch('https://api.graph.cool/relay/v1/cj6r44cg91gro0152uxqt05q7', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      query: operation.text,
+      variables,
+    }),
+  }).then(response => {
+    return response.json()
+  })
+})
+
+const environment = new Environment({
+  network,
+  store,
+})
+
+export default environment
